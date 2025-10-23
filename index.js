96f5bd149659425c13678f650380a3e4d735f22b
masudmolla6 <masudmolla2937@gmail.com>
2025-10-23 13:16:20 +0600

    create specific product api

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express");
 require("dotenv").config();
 const cors = require("cors");
 const app = express();
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const port = process.env.PORT || 5000;
 
 
@@ -111,6 +111,14 @@ async function run() {
     res.status(500).json({ message: "Internal Server Error" });
   }
 });
+
+
+  app.get("/products/productDetails/:id", async(req, res)=>{
+    const id=req.params.id;
+    const query={_id:new ObjectId(id)};
+    const result=await productsCollections.findOne(query);
+    res.send(result);
+  })
 
 
 
