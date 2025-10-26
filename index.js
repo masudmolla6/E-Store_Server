54d10c78ab36cf846c86b1654ef0c75d3505487c
masudmolla6 <masudmolla2937@gmail.com>
2025-10-26 19:44:58 +0600

    integrate jwt

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require("express");
 require("dotenv").config();
 const cors = require("cors");
 const app = express();
+const jwt=require("jsonwebtoken");
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const port = process.env.PORT || 5000;
 
@@ -32,6 +33,15 @@ async function run() {
     const userCollections=client.db("E-Store").collection("users");
     const cartsCollections=client.db("E-Store").collection("carts");
 
+
+    // jwt related api
+    app.post("/jwt", async(req, res)=>{
+      const user=req.body;
+      const token=jwt.sign(user, process.env.ACCESS_TOKEN_SECRET,{
+        expiresIn:"1h"
+      });
+      res.send({token});
+    })
 
 
     // User Relative Api
