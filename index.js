e00b1b402fe3fe5e51376403cd58d91358e8e077
masudmolla6 <masudmolla2937@gmail.com>
2026-01-01 19:14:06 +0600

    try to updated product

--- index.js
+++ index.js
@@ -228,6 +228,10 @@ async function run() {
       res.send(result);
     })
 
+    app.patch("/products", async(req, res)=>{
+      const id=req.params.id;
+      const query={_id:new ObjectId(id)};
+    })
 
 
 
